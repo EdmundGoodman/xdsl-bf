9a4ac1b5bd5e14470c564b84ec550a32bb07a791
EdmundGoodman <egoodman3141@gmail.com>
2025-06-20 18:40:41 +0100

    Add test for bf dialect

---
+++ tests/test_bf_dialect.py
@@ -0,0 +1,39 @@
+"""Unit tests for the bf dialect."""
+
+from xdsl.builder import ImplicitBuilder
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.ir import Block, Region
+
+from xdslbf.dialects import bf
+
+
+def test_bf_builder() -> None:
+    """Test building and printing all operations in the bf dialect."""
+    module = ModuleOp([])
+    with ImplicitBuilder(module.body):
+        bf.InOp()
+        bf.IncOp()
+        loop = bf.LoopOp(regions=[Region([Block([])])])
+        with ImplicitBuilder(loop.body):
+            bf.RshftOp()
+            bf.DecOp()
+            bf.DecOp()
+            bf.LshftOp()
+            bf.RetOp()
+        bf.RshftOp()
+        bf.OutOp()
+    expected = """\
+builtin.module {
+  "bf.in"() : () -> ()
+  "bf.inc"() : () -> ()
+  "bf.loop"() ({
+    "bf.rshft"() : () -> ()
+    "bf.dec"() : () -> ()
+    "bf.dec"() : () -> ()
+    "bf.lshft"() : () -> ()
+    "bf.ret"() : () -> ()
+  }) : () -> ()
+  "bf.rshft"() : () -> ()
+  "bf.out"() : () -> ()
+}"""
+    assert str(module) == expected
